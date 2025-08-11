03059a6b6c9dc25e3e68c34b9b29fb3e7938e1a2
Jayne Gomes <jaygmorais@gmail.com>
2025-08-10 23:20:45 -0300

    feat: add category products page

---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,46 @@
+import { eq } from "drizzle-orm";
+import { notFound } from "next/navigation";
+
+import Footer from "@/components/common/footer";
+import { Header } from "@/components/common/header";
+import ProductItem from "@/components/common/product-item";
+import { db } from "@/db";
+import { categoryTable, productTable } from "@/db/schema";
+
+interface CategoryPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const CategoryPage = async ({ params }: CategoryPageProps) => {
+  const { slug } = await params;
+  const category = await db.query.categoryTable.findFirst({
+    where: eq(categoryTable.slug, slug),
+  });
+
+  if (!category) {
+    return notFound();
+  }
+  const products = await db.query.productTable.findMany({
+    where: eq(productTable.categoryId, category.id),
+    with: {
+      category: true,
+      variants: true,
+    },
+  });
+  return (
+    <>
+      <Header />
+      <div className="space-y-6 px-5">
+        <h2 className="py-6 text-xl font-semibold">{category.name}</h2>
+        <div className="grid grid-cols-2 gap-2">
+          {products.map((product) => (
+            <ProductItem key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default CategoryPage;
--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-accent w-full gap-1 p-8">
+    <div className="bg-accent mt-6 w-full gap-1 p-8">
       <p className="text-ts font-medium">ⓒ 2025 Copyright Dragão Mercante</p>
       <p className="text-muted-foreground text-xs font-medium">
         Todos os direitos reservados.
